f77609b270ff30d7c13a574f4a199803d39a408f
Dmytro Malakhov <byuarus@gmail.com>
2026-06-26 16:46:19 -0700

    Fixed issue when we stopped receiving captureInfo after memory storage reformat and reconnect

--- src/mavsdk/plugins/camera/camera_impl.h
+++ src/mavsdk/plugins/camera/camera_impl.h
@@ -145,6 +145,7 @@ private:
     void notify_status_locked();
 
     void check_status();
+    void reset_capture_info();
 
     bool should_fetch_camera_definition(const std::string& uri) const;
     bool fetch_camera_definition(
